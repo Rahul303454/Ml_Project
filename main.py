8c38984c7063ab261a89e985c9ff78bc236b3bdc
Rahul Kumar <rahulk49855@gmail.com>
2026-09-04 23:05:51 +0530

    Serve frontend through FastAPI

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 import joblib
 import pandas as pd
+import os
+
 from pydantic import BaseModel, Field
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
 
 app = FastAPI()
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,48 +19,79 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+# Get current directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 COLUMNS = [
-    'latitude',
-    'longitude',
-    'price',
-    'minimum_nights',
-    'number_of_reviews',
-    'reviews_per_month',
-    'calculated_host_listings_count',
-    'availability_365',
-    'neighbourhood_group',
-    'neighbourhood'
+    "latitude",
+    "longitude",
+    "price",
+    "minimum_nights",
+    "number_of_reviews",
+    "reviews_per_month",
+    "calculated_host_listings_count",
+    "availability_365",
+    "neighbourhood_group",
+    "neighbourhood"
 ]
 
-model = joblib.load('Model_Pipeline.pkl')
+
+# Load model
+model = joblib.load(
+    os.path.join(
+        BASE_DIR,
+        "Model_Pipeline.pkl"
+    )
+)
 
 
 class Features(BaseModel):
+
     latitude: float = Field(..., ge=-90, le=90)
+
     longitude: float = Field(..., ge=-180, le=180)
+
     price: float = Field(..., ge=0)
+
     minimum_nights: int = Field(..., ge=1, le=365)
+
     number_of_reviews: int = Field(..., ge=0)
+
     reviews_per_month: float = Field(..., ge=0)
+
     calculated_host_listings_count: int = Field(..., ge=0)
+
     availability_365: int = Field(..., ge=0, le=365)
+
     neighbourhood_group: str = Field(..., min_length=1)
+
     neighbourhood: str = Field(..., min_length=1)
 
 
+# Serve Frontend
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+
+    return FileResponse(
+        os.path.join(
+            BASE_DIR,
+            "index.html"
+        )
+    )
 
 
 @app.post("/predict")
 def predict(features: Features):
+
     row = pd.DataFrame(
         [features.model_dump()],
         columns=COLUMNS
     )
 
     prediction = model.predict(row)
+
     probability = model.predict_proba(row)
 
     return {
